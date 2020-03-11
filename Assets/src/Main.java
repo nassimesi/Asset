89cc9ccd97690e61df7a22830140f77637080b95
nassimesi <fn_adrao@esi.dz>
2020-03-11 21:13:44 +0100

    get value as string

--- Assets/src/Main.java
+++ Assets/src/Main.java
@@ -1,13 +1,13 @@
 import spoon.Launcher;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.path.CtRole;
+import spoon.support.reflect.declaration.CtFieldImpl;
 
 public class Main {
 
     public static void main(String[] args) {
         CtClass l = Launcher.parseClass("class A {public int a=1; void m() { System.out.println(\"yeah\"+a);} }");
-        System.out.println(l.getDirectChildren().get(1).getRoleInParent().toString());
-
+        System.out.println(((CtFieldImpl)(l.getDirectChildren().get(1))).getDefaultExpression().toString());
 
     }
 }
