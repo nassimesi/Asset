1b76c8e284294a624f7c321e166541fb7db087a1
nassimesi <fn_adrao@esi.dz>
2020-03-11 20:59:13 +0100

    Add attributes to Asset Class

--- Assets/src/Asset.java
+++ Assets/src/Asset.java
@@ -1,4 +1,17 @@
+import spoon.reflect.declaration.CtElement;
+
 public class Asset {
+    private static int autoIncrement=0;
     private String nom;
+    private String value;
+    private String type;
+    private CtElement parent;
+    private int id = 0;
+
+    public Asset(){
+        autoIncrement++;
+        id = autoIncrement;
+        
+    }
 
 }
