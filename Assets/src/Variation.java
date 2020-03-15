bd5bb0dc6e5c85b17950c92e1618bf12c891a47e
nassimesi <fn_adrao@esi.dz>
2020-03-15 22:01:08 +0100

    StackOverflow Error to fix

--- Assets/src/Variation.java
+++ Assets/src/Variation.java
@@ -1,13 +1,31 @@
-public abstract class Variation {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Variation {
     private String type; // Type de variation, soit classe, soit méthode , soit un attribut
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name; // Le nom de la variante
+    public static ArrayList<Variation> listVariation = new ArrayList<>();
+    private ArrayList<Asset> relatedAssets;
     /**
      * *
      * @return Une chaîne de caractère contenant le type de variation
      * @see Asset#getPossibleTypes()
      */
+
+
     public String getType(){
         return type;
+    }
+
+    public Variation(String nom, String type){
+        name = nom;
+        this.type = type;
+        this.relatedAssets = new ArrayList<>();
     }
 
     /**
@@ -18,4 +36,21 @@ public abstract class Variation {
         this.type = type;
     }
 
+    public void addAsset(Asset e){
+        if (!relatedAssets.contains(e)) relatedAssets.add(e);
+    }
+
+    public static void addVariation(Variation e){
+        if (!listVariation.contains(e)) listVariation.add(e);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (this.name.equals(((Variation)obj).name) && this.type.equals((((Variation)obj).type)));
+    }
+
+    @Override
+    public String toString() {
+        return "type = "+this.type +" and name = "+name+"and the related assets are :"+relatedAssets+"\n";
+    }
 }
