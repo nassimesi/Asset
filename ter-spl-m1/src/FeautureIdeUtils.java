7b1c3a9b538315bea4a7f862250d72d72a4a7a85
ADRAO Nassim <fn_adrao@esi.dz>
2020-04-03 22:20:01 +0200

    ENOVBM : merge with other works

---
+++ ter-spl-m1/src/FeautureIdeUtils.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.util.Arrays;
+
+import spoon.reflect.path.CtRole;
+
+public abstract class FeautureIdeUtils {
+	public static void createPackageForClasses(Asset e) {
+		if (e.getType().equals("classe")) {
+			String path = e.getParent().prettyprint()+""; 
+			System.out.println("list of folders "+Arrays.toString(new File(".").listFiles()));
+			if(!Arrays.asList(new File(".").listFiles()).contains(new File(".\\"+e.getParent().getValueByRole(CtRole.NAME)+""))) {
+				System.out.println("3la chati2a lmatbakh"+e.getParent().getValueByRole(CtRole.NAME).toString().equals("unnamed package"));
+				new File(".\\cluster\\"+(e.getParent().getValueByRole(CtRole.NAME).toString().equals("unnamed package")?"source":(e.getParent().getValueByRole(CtRole.NAME)+""))).mkdirs();
+			}
+		}
+	};
+}
